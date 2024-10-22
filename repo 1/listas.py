7f77c0eb83c1d9aa8fbf0000ef67c1d8ed37af29
Ticonaso <aronticona5@gmail.com>
2024-10-22 12:21:26 -0500

    apredizaje :v

---
+++ repo 1/listas.py
@@ -0,0 +1,12 @@
+lista = ["python",53,False,23.3]
+
+lista.append("wa")
+
+lista.insert(1,"wasa")
+print(lista)
+
+lista.remove(23.3)
+print(lista)
+
+lista.pop(3)
+print(lista)
---
+++ repo 1/wa.py
@@ -0,0 +1,8 @@
+print("hola mundo")
+def suma(a,b):
+    c = a + b
+    return c
+
+
+
+print(suma(10,20))
